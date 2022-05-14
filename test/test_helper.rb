03770eac700fc61b02666bad94a5ace4bf601ab4
a2238rh <a2238rh@aiit.ac.jp>
2022-05-14 07:41:04 +0000

    Finish user edit, update, index, and destroy actions

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -18,5 +18,12 @@ class ActiveSupport::TestCase
     !session[:user_id].nil?
   end
   
+  # テストユーザーとしてログインする
+  def log_in_as(user, password: 'password', remember_me: '1')
+    post login_path, params: { session: { email: user.email,
+                                          password: password,
+                                          remember_me: remember_me } }
+  end
+  
   include ApplicationHelper
 end
