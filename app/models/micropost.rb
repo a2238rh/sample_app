e7c6beea62f9c103b9e4be74fc10410222b5f0bb
a2238rh <a2238rh@aiit.ac.jp>
2022-05-15 02:27:32 +0000

    fix: check image before showing

--- app/models/micropost.rb
+++ app/models/micropost.rb
@@ -1,8 +1,7 @@
 class Micropost < ApplicationRecord
-  belongs_to :user
+  belongs_to       :user
   has_one_attached :image
   default_scope -> { order(created_at: :desc) }
-
   validates :user_id, presence: true
   validates :content, presence: true, length: { maximum: 140 }
   validates :image,   content_type: { in: %w[image/jpeg image/gif image/png],
@@ -13,5 +12,9 @@ class Micropost < ApplicationRecord
   # 表示用のリサイズ済み画像を返す
   def display_image
     image.variant(resize_to_limit: [500, 500])
+  end
+
+  def image_available?
+    image.attached? && image.variable?
   end
 end
